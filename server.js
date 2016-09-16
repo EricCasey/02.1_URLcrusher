134f3557448562903bf5ecf1fc16f70ff8ee6060
Eric Casey <ericcasey509@gmail.com>
2016-09-16 17:32:05 +0000

    fall cleaning

--- server.js
+++ server.js
@@ -8,78 +8,93 @@ const methodOverride = require('method-override');
 const bodyParser = require("body-parser");
 
 let urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
-  "2whg3g": "http://www.pornhub.com"
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com",
+    "2whg3g": "http://www.pornhub.com"
 };
+
+//
+// const MongoClient = require("mongodb").mongoClinet.
+// const MONGODB_URL = "";
+//
+
+
+
+// app.get("....", (req, res) => {
+//     Mongo.urls.find("e24e2w").toArray(function(err, result) {
+//         res.render("template_page", {
+//             url: result
+//         })
+//     });
+// });
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 // split body?  depricated.
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 // set static directory for accessing css files
 app.use(express.static(__dirname + '/'))
-//
+    //
 app.use(methodOverride('_method'))
 
 
 //index page
 app.get('/', (req, res) => {
-  //  res.render('urls_index', {    urls: urlDatabase  });
-   res.render("urls_new");
+    //  res.render('urls_index', {    urls: urlDatabase  });
+    res.render("urls_new");
 });
 
 // +++++   URL LIST PAGE  +++++++++
 // URL list page
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+    let templateVars = {
+        urls: urlDatabase
+    };
+    res.render("urls_index", templateVars);
 });
 
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  //res.send("Ok");
-  let templateVars = { shortURL: req.params.id , urls: urlDatabase };
-
-  let newID = generateRandomString(6);
-  console.log(newID)
-  res.render("urls_show", templateVars);
+    console.log(req.body); // debug statement to see POST parameters
+    let newID = generateRandomString(6);
+    console.log(newID)
+    urlDatabase[newID] = req.body.longURL;
+    res.redirect("/urls");
 
 });
 
 app.delete('/urls/:id', (req, res) => {
-
-  delete urlDatabase[req.params.id];
-  res.redirect('/urls');
-  console.log(`someone deleted ${req.params.id}`);
-
+    delete urlDatabase[req.params.id];
+    res.redirect('/urls');
+    console.log(`someone deleted ${req.params.id}`);
 });
 
 app.put('/urls/:id', (req, res) => {
-//replace(urlDatabase[req.params.id])
-let newID = generateRandomString(6);
+    //replace(urlDatabase[req.params.id])
+    let newID = generateRandomString(6);
 
-if (urlDatabase.hasOwnProperty(req.params.id)) {
-    urlDatabase[newID] = urlDatabase[req.params.id];
-    delete urlDatabase[req.params.id];
-}
-  res.redirect('/urls');
+    if (urlDatabase.hasOwnProperty(req.params.id)) {
+        urlDatabase[newID] = urlDatabase[req.params.id];
+        delete urlDatabase[req.params.id];
+    }
+    res.redirect('/urls');
 });
 
 
 // +++++   FORM PAGE  +++++++++
 // setting up for the /new form
 app.get("/urls/new", (req, res) => {
- res.render("urls_new");
+    res.render("urls_new");
 });
 
 
 // +++++   THIS REDIRECTS TO DESTINATION  +++++++++
 app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-  console.log("Someone Used A Link!")
+    let longURL = urlDatabase[req.params.shortURL];
+    res.redirect(longURL);
+    console.log("Someone Used A Link!")
 });
 // must have http://www.
 //
@@ -87,8 +102,11 @@ app.get("/u/:shortURL", (req, res) => {
 
 // indiv
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id , urls: urlDatabase };
-  res.render("urls_show", templateVars);
+    let templateVars = {
+        shortURL: req.params.id,
+        urls: urlDatabase
+    };
+    res.render("urls_show", templateVars);
 });
 
 
@@ -101,7 +119,7 @@ console.log('Server open on :8080');
 function generateRandomString(len) {
     var text = " ";
     var charset = "abcdefghijklmnopqrstuvwxyz0123456789POIUYTREWQLKJHGFDSAMNBVCXZ";
-    for( var i=0; i < len; i++ )
+    for (var i = 0; i < len; i++)
         text += charset.charAt(Math.floor(Math.random() * charset.length));
     return text;
 }
